880ae20a463ac96794f60449ec4be7d71af15e27
pamela vieira da silva <panhavsilva@gmail.com>
2022-04-08 09:14:17 -0300

    feat: configura fastify

---
+++ src/ports/fastify/index.ts
@@ -0,0 +1,19 @@
+import fastifyCors from 'fastify-cors'
+import fastify from 'fastify'
+
+const app = fastify({logger: true})
+
+app.register(fastifyCors, { origin: true })
+
+app.get('/', (_request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+export const start = async () => {
+  try {
+    await app.listen({port: +(process.env['PORT'] ?? 3000)})
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
