ed7002598bd9e97269784552a2a744eeed2a1c06
pamela vieira da silva <panhavsilva@gmail.com>
2022-04-11 15:32:02 -0300

    :sparkles: feat: inclui express config

--- src/ports/express/index.ts
+++ src/ports/express/index.ts
@@ -1,13 +1,26 @@
-import express from 'express'
+import express, { Response, Request, Router } from 'express'
 import cors from 'cors'
+const routes = Router()
+
+routes.get('/', function (_req: Request, res: Response) {
+  return res.render('hello')
+})
 
 const app = express()
 const PORT = 'PORT'
+const CORS_ORIGINS = 'CORS_ORIGINS'
+const corsOptions = {
+  origin: process.env[CORS_ORIGINS],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSucessStatus: 204,
+}
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.use(cors())
+app.use(cors(corsOptions))
+app.use(routes)
 
 export function start () {
   return new Promise((resolve) => {
---
+++ src/ports/express/modules/products.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import { createErrorMessage } from '@/ports/express/helpers'
+
+import { prisma } from '@/ports/prisma'
+
+export const getSingleProduct = async (req: Request, res: Response) => {
+  const { id } = req.params
+  try {
+    const singleProduct = await prisma.products.findUnique({
+      where: { id },
+    })
+
+    if (singleProduct === null) {
+      return res.status(404)
+        .json(createErrorMessage('Product not found!'))
+    }
+
+    return res.json(singleProduct)
+  } catch (error) {
+    console.log('Error: ', error)
+
+    return res.status(400)
+      .json(createErrorMessage('Produto não encontrado!'))
+  }
+}
